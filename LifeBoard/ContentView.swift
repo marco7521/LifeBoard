b509f52fdba3e70357b9203cd68249f76d64e571
金融產規部財工組-鄭勝文 <marco@mitake.com.tw>
2025-12-01 18:01:08 +0800

    Initial project

---
+++ LifeBoard/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LifeBoard
+//
+//  Created by 金融產規部財工組-鄭勝文 on 2025/12/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LifeBoard/LifeBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeBoardApp.swift
+//  LifeBoard
+//
+//  Created by 金融產規部財工組-鄭勝文 on 2025/12/1.
+//
+
+import SwiftUI
+
+@main
+struct LifeBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
